27f553cc4c604c97e8e40334298bb9eeb5123604
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-08-21 15:30:30 +0300

    Create main.py

---
+++ project-02-oracle-database-management/app/main.py
@@ -0,0 +1,17 @@
+from app.logging_config import configure_logging
+from app.config import get_settings
+from app.ui.dashboard import run_dashboard
+
+
+def main():
+    settings = get_settings()
+
+    configure_logging(
+        settings.log_level
+    )
+
+    run_dashboard()
+
+
+if __name__ == "__main__":
+    main()
