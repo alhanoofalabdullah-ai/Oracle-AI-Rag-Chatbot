cb3e2541f25285b21088fc50656478f13c22aa02
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-08-22 21:15:19 +0300

    Create health.py

---
+++ project-07-oracle-enterprise-api/api/routers/health.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    tags=["Health"]
+)
+
+
+@router.get(
+    "/health"
+)
+def health():
+
+    return {
+
+        "status":
+            "healthy",
+
+        "service":
+            "oracle-enterprise-api"
+
+    }
